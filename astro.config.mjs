dc5afe89c4e31b501a845c100372bcbff2398275
eriknorris <155327355+eriknorris@users.noreply.github.com>
2026-08-27 08:58:01 -0700

    feat: publish issue 199 editorial decompositions (#209)

--- astro.config.mjs
+++ astro.config.mjs
@@ -60,6 +60,9 @@ const __dirname = path.dirname(__filename);
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
+	redirects: {
+		"/projects/zeus": "/projects/webtv-elmer",
+	},
 	// Cloudflare Adapter handles the edge image service / Worker generation.
 	adapter: cloudflare({
 		imageService: "compile",
